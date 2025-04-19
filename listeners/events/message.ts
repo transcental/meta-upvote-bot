ac73e64e399b3fbd30cf3e9facb0bceb0e24283a
Amber <amber@transcental.dev>
2025-04-19 15:02:39 +0100

    support file upload messages

--- listeners/events/message.ts
+++ listeners/events/message.ts
@@ -6,7 +6,7 @@ const newMessage = async ({ client, event }: {client: WebClient, event: any}) =>
   if (
     event.channel === process.env.CHANNEL_ID &&
     !event.thread_ts &&
-    !event.subtype
+    (!event.subtype || ["file_share", "me_message"].includes(event.subtype))
   ) {
     messagePosted({ client, event });
   } else if (event.channel === process.env.CHANNEL_ID && event.subtype == "message_changed" && event.message.subtype == "tombstone") {
